0fab495b538be4328c1b8c69f45b59cfec438c2d
Reyan <dinreyanud@gmail.com>
2022-10-31 11:37:54 +0000

    added user and age

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Box from './components/box';
+import {useState} from 'react'
+
+function App() {
+  const [user, setUser] = useState("");
+  const [age, setAge] = useState();
+  return (
+    <div className="App">
+      <input onChange={(event) => setUser(event.target.value)}/>
+      <input onChange={(event) => setAge(event.target.value)}/>
+      {user ? <div> <h1>Harry Potter</h1>
+      <Box name="George Weasly" age="17"/>
+      <Box name="Hermione Grainger" age="15"/>
+      <Box name="Rubeus Hagrid" age="47"/>
+      <Box name={user} age={age}/>
+      </div>
+      :<div>
+        <h1>the is where the else if statment is written</h1>
+        </div>}
+    </div>
+  );
+}
+
+export default App;
